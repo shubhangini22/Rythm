98595dfd82756802b62b3e177818cff6baab9340
Shubhangini Singh <shubhangini2203@gmail.com>
2023-03-12 12:34:21 +0530

    Add files via upload

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color(0xFF52EE96);
+
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color(0xFF645F5F);
+
+const Color tdBGColor = Color(0xFFA4AAAF);
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import './screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    //transparent color for the status bar mobile
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //to get rid of the debug text on the top
+      title: 'ToDo App',
+      home: Home(),
+    );
+  }
+}
+
+
